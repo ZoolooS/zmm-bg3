97b995678df802a9242396c4546c1f864c1733cd
Ilya ZoolooS Popov <zooloosik@gmail.com>
2023-09-21 08:15:46 +0300

    Added stub of application.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # zmm-bg3
 ZoolooS's mods merger for BG3
+
+
+---
+- Copyright © 2023 Ilya Popov aka ZoolooS. All rights reserved.
+- Author: Ilya Popov aka ZoolooS
+- Description: This file is the part of "ZoolooS's mods merger for BG3" application
+---
---
+++ main.py
@@ -0,0 +1,12 @@
+################################################################################
+# Copyright © 2023 Ilya Popov aka ZoolooS. All rights reserved.                #
+# Author: Ilya Popov aka ZoolooS                                               #
+# Description:                                                                 #
+#     This file is the part of "ZoolooS's mods merger for BG3" application     #
+################################################################################
+def main(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    main('Boo')
