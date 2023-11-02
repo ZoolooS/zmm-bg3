387dbdba883ee03a0d72364d656e64d66c0f500a
Ilya ZoolooS Popov <zooloosik@gmail.com>
2023-11-02 05:06:19 +0300

    Add some TODOs. Update some dependencies.

--- bin/archivers.py
+++ bin/archivers.py
@@ -4,12 +4,111 @@
 # Description:                                                                 #
 #     This file is the part of "ZoolooS's mods merger for BG3" application     #
 ################################################################################
+from pathlib import Path
 import shutil
 import zipfile
 
 from bin.consts import AppConsts
 
 
+# TODO: Реализовать классы (
+#           Mod,
+#           ModArchived,
+#           ModUnarchived,
+#           ModPaked,
+#           ModUnpaked,
+#           Archiver,
+#           Paker
+#       ) и вынести в нужные модули (если нужно)
+class Mod:
+    def __init__(self,
+                 name,
+                 file_name,
+                 mod_path,
+                 path_to_destination):
+        self.name = name
+        self.file_name = file_name
+        self.mod_path = mod_path
+        self.path_to_destination = path_to_destination
+
+
+class Archiver:
+    def __init__(self, path_to_archiver, archiver_type):
+        self.path_to_archiver = path_to_archiver
+        self.archiver_type = archiver_type
+
+    def path(self):
+        return self.path_to_archiver
+
+    def arch_type(self):
+        return self.archiver_type
+
+
+class ModArchived(Mod):
+    def __init__(self,
+                 name,
+                 file_name,
+                 mod_path,
+                 path_to_destination,
+                 arch_type):
+        super().__init__(name, file_name, mod_path, path_to_destination)
+        self.arch_type = arch_type  # 'zip', '7z', 'rar'
+        # path_to_archive = ''  # Mod.mod_path
+        # path_to_destination = ''  # Mod.path_to_destination
+
+    def unpack_archive(self, arch_type, path_to_archiver, path_to_archive, path_to_destination):
+        if arch_type == 'zip':
+            self.unpack_zip(path_to_archiver, path_to_archive, path_to_destination)
+        if arch_type == '7z':
+            self.unpack_7z(path_to_archiver, path_to_archive, path_to_destination)
+        if arch_type == 'rar':
+            self.unpack_rar(path_to_archiver, path_to_archive, path_to_destination)
+        else:
+            pass  # TODO: Rise Exception
+
+    def unpack_zip(self, path_to_archiver, path_to_archive, path_to_destination):
+        if path_to_archiver:
+            self.unpack_zip_c(path_to_archiver, path_to_archive, path_to_destination)
+            # TODO: and exit
+
+        shutil.unpack_archive(path_to_archive, path_to_destination)
+        # or
+        # with zipfile.ZipFile(path_to_archive, 'r') as zip_ref:
+        #     zip_ref.extractall(path_to_destination)
+
+    def unpack_zip_c(self, path_to_archiver, path_to_archive, path_to_destination):
+        pass
+
+    def unpack_7z(self, path_to_archiver, path_to_archive, path_to_destination):
+        pass
+
+    def unpack_rar(self, path_to_archiver, path_to_archive, path_to_destination):
+        pass
+
+
+class ModUnarchived(Mod):
+    def __init__(self):
+        arch_type = ''  # 'zip', '7z', 'rar'
+        # path_to_source = ''  # Mod.path
+        # path_to_destination = ''  # Mod.path_to_destination
+
+
+class Paker:
+    """ Mod-files for BG3 is *.pak files, so class name is Paker not Packer. """
+    def __init__(self):
+        path_to_paker = ''
+
+
+class ModPaked(Mod):
+    pass
+
+
+class ModUnpaked(Mod):
+    pass
+
+
+# TODO: Классы ArchiveUnpacker, ArchivePacker, PakUnpacker, PakPacker переделать в классы
+#       Mod, ModArchived, ModUnarchived, ModPaked, ModUnpaked, Archiver, Paker
 class ArchiveUnpacker:
     arch_type = ''  # 'zip', '7z', 'rar'
     path_to_archiver = ''
@@ -52,15 +151,23 @@ class ArchivePacker:
     path_to_source = ''
     path_to_destination = ''
 
-    def pack_archive(self, arch_type, path_to_archiver, path_to_source, path_to_destination):
-        pass
+    def pack_archive(self, arch_type, path_to_archiver, path_to_source, path_to_destination, archive_name='archive'):
+        if arch_type == 'zip':
+            self.pack_zip(path_to_archiver, path_to_source, path_to_destination, archive_name)
+        if arch_type == '7z':
+            self.pack_7z(path_to_archiver, path_to_source, path_to_destination)
+        if arch_type == 'rar':
+            self.pack_rar(path_to_archiver, path_to_source, path_to_destination)
+        else:
+            pass  # TODO: Rise Exception
 
-    def pack_zip(self, path_to_archiver, path_to_source, path_to_destination):
+    def pack_zip(self, path_to_archiver, path_to_source, path_to_destination, archive_name):
         if path_to_archiver:
             self.pack_zip_cmd(path_to_archiver, path_to_source, path_to_destination)
             # TODO: and exit
 
-        shutil.make_archive(path_to_source, path_to_destination)
+        # shutil.make_archive(archive_name, 'zip', path_to_source, path_to_destination)
+        shutil.make_archive(archive_name, 'zip', path_to_source)
         # or
         # with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
         #     zip_ref.extractall(directory_to_extract_to)
--- main.py
+++ main.py
@@ -5,31 +5,62 @@
 #     This file is the part of "ZoolooS's mods merger for BG3" application     #
 ################################################################################
 import json
+import time
 from os import listdir
 from os.path import isfile, join
+from pathlib import Path
 
 from bin.consts import AppConsts
 from bin.archivers import *
 
 
 def main(name):
+    # TODO: It need's to be just runner for:
+    #       - read settings
+    #       - set settings (when GUI will appears)
+    #       - run unarch process: - get next archived mod ->
+    #                             - unarch to destination (to unique folder name each) ->
+    #                             - repeat
+    #       - run unpak process: - get next pak file ->
+    #                            - unpak to destination (to unique folder name each) ->
+    #                            - move/copy settings json to destination if exist ->
+    #                            - repeat
+    #       - run merge process: - get old Result mod or create empty Result mod ->
+    #                            - get next unpaked mod (sets of folders and files) ->
+    #                            - merge mod to Result mod (with rewrite old mod datastrings or ask user what to do) ->
+    #                            - merge mod names to Result mod settings file ->
+    #                            - merge mod settings to Result mod settings if needed ->
+    #                            - repeat
+    #       - run pak process: - get Result mod folder ->
+    #                          - pak Result mod folder to *.pak
+    #       - place *.pak to game mod folder (optional)
+    #       - place unpaked Result mod to game raw mod folder (optional)
+    #       - clear all tmp files and folders (optional)
+
     print(f'Hi, {name}')
 
     CONSTS = AppConsts()
     print(f'{CONSTS.SETTINGS["FILE_NAME_BKP"]}.{CONSTS.SETTINGS["FILE_TYPE"]}')
 
     # Archives
-    # some code
     print('Unpacking start!!!!!!!')
-    # unpack = ArchiveUnpacker(arch_type, path_to_archiver, path_to_archive, path_to_destination)
-
     source = f'{CONSTS.DIRS["WD"]}/{CONSTS.DIRS["WD_IN"]}'
     destination = f'{CONSTS.DIRS["WD"]}/{CONSTS.DIRS["WD_OUT"]}'
     onlyfiles = [f for f in listdir(source) if isfile(join(source, f)) and f != '.gitignore']
     for file in onlyfiles:
-        archive = ArchiveUnpacker()
-        archive.unpack_archive('zip', None, f'{source}/{file}', f'{destination}/{file[:-4]}')
+        archiveUnpack = ArchiveUnpacker()
+        archiveUnpack.unpack_archive('zip', None, f'{source}/{file}', f'{destination}/{file[:-4]}')
+    print('Done!!!!!!!')
 
+    time.sleep(5)
+
+    print('Packing start!!!!!!!')
+    source = f'{CONSTS.DIRS["WD"]}/{CONSTS.DIRS["WD_OUT"]}'
+    destination = f'{CONSTS.DIRS["WD"]}/{CONSTS.DIRS["WD_TMP"]}'
+    dirs = [d for d in listdir(source) if d != '.gitignore']
+    for dir_ in dirs:
+        archivePack = ArchivePacker()
+        archivePack.pack_archive('zip', None, f'{source}/{dir_}', destination, dir_)
     print('Done!!!!!!!')
 
 
